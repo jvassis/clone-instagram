7614e2dc264243d313ba851d0574bc571a1d268c
jvassis <jackson.assis07@gmail.com>
2023-04-16 10:58:03 -0300

    adicao de consumo de api para parte de sugestoes

--- src/components/Suggestion/index.js
+++ src/components/Suggestion/index.js
@@ -1,6 +1,20 @@
+import { useEffect, useState } from 'react'
 import './style.css'
 
 export function Suggestion() {
+    const [suggestions, setSuggestions] = useState([])
+    const [limitUsers, setLimitUsers] = useState(5)
+
+    const slice = suggestions.slice(0, limitUsers)
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/gabrieldiasss/followers')
+            .then(response => response.json())
+            .then(result => setSuggestions(result))
+            .catch(err => { throw new Error(err) })
+            .then(() => suggestions.map(suggestion => console.log(suggestion.login)))
+    }, [])
+
     return (
         <div className='container-suggestion'>
             <div className="header-suggestion">
@@ -18,14 +32,16 @@ export function Suggestion() {
                 <span>Ver tudo</span>
             </div>
             <div className='user-suggestion'>
-                <div className='infos-suggestion'>
-                    <img src='https://github.com/gabrieldiasss.png' />
-                    <div className='info-suggestion'>
-                        <span>gbrldiass</span>
-                        <p>Seguido por filipedechamps</p>
+                {slice.map((suggestion, key) => (
+                    <div className='infos-suggestion' key={key}>
+                        <img src={`https://github.com/${suggestion.login}.png`} />
+                        <div className='info-suggestion'>
+                            <span>{suggestion.login}</span>
+                            <p>Seguido por filipedechamps</p>
+                        </div>
+                        <button className='follow'>Seguir</button>
                     </div>
-                    <button className='follow'>Seguir</button>
-                </div>
+                ))}
             </div>
             <footer className='footer-suggestion'>
                 <p>Sobre &bull; Ajuda &bull; Imprensa &bull; API &bull; Carreiras &bull; Privacidade &bull; Termos &bull; Localizações &bull; Principais contas &bull; Hashtags &bull; idioma</p>
